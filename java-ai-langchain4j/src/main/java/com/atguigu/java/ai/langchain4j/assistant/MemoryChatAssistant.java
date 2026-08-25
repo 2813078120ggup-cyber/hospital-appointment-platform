bfeac5636b5a044dd581d314451ff799e031453e
gaochao <gaochao_dev@qq.com>
2026-08-25 17:06:35 +0800

    完成langchain4j前置学习测试

--- java-ai-langchain4j/src/main/java/com/atguigu/java/ai/langchain4j/assistant/MemoryChatAssistant.java
+++ java-ai-langchain4j/src/main/java/com/atguigu/java/ai/langchain4j/assistant/MemoryChatAssistant.java
@@ -1,5 +1,6 @@
 package com.atguigu.java.ai.langchain4j.assistant;
 
+import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
 
 import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
@@ -10,5 +11,6 @@ import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
     chatMemory = "chatMemory"
 )
 public interface MemoryChatAssistant {
+	@UserMessage("你是我的好朋友，请用上海话回答问题，并且添加一些表情符号。 {{it}}") //{{it}}表示这里
 	String chat(String message);
 }
